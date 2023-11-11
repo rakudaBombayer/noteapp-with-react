296e7325d63d427b46177be12a6d5be398e2392c
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-11-11 16:00:18 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+import Sidebar from './components/Sidebar'
+import Main from './components/Main'
+import { useState } from 'react';
+import uuid from 'react-uuid';
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    console.log("新しくノートが追加されました。");
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  };
+
+  return (
+    <>
+    <div className="App">
+      <Sidebar onAddNote={onAddNote} 
+      notes={notes} 
+      onDeleteNote={onDeleteNote}  />
+      <Main />  
+    </div>
+    </>
+  )
+}
+
+export default App;
